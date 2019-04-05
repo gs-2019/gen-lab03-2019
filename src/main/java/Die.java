408ae118b20951b54d07180679bef93415682fcb
Nemanja Pantic <nemo.pantic@gmail.com>
2019-04-05 11:47:37 +0200

    Implememted dice and diceTest

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,2 +1,24 @@
+import java.util.Random;
+
 public class Die {
+
+    private int faceValue;
+
+    Die(){
+        this.faceValue = 0;
+    }
+
+    /*
+    *
+    * */
+    public void throwDie(){
+        int temp = 0;
+        Random rand = new Random();
+        //Generate a random number between 1-6
+        this.faceValue = rand.nextInt(6) + 1;
+    }
+
+    public int getValue(){
+        return this.faceValue;
+    }
 }
