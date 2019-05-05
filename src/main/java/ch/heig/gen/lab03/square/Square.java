a2b07124f97eab869611efb1653d7387148c7bf3
nyg <nyg@users.noreply.github.com>
2019-05-05 11:34:18 +0200

    Issue #18, added equals and hashCode methods to Square class, added test to PieceTest.

--- src/main/java/ch/heig/gen/lab03/square/Square.java
+++ src/main/java/ch/heig/gen/lab03/square/Square.java
@@ -2,6 +2,8 @@ package ch.heig.gen.lab03.square;
 
 import ch.heig.gen.lab03.Player;
 
+import java.util.Objects;
+
 public abstract class Square {
 
     private int position;
@@ -14,5 +16,25 @@ public abstract class Square {
 
     public int getPosition() {
         return this.position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Square square = (Square) o;
+        return position == square.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
--- src/test/java/ch/heig/gen/lab03/PieceTest.java
+++ src/test/java/ch/heig/gen/lab03/PieceTest.java
@@ -1,7 +1,18 @@
 package ch.heig.gen.lab03;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ch.heig.gen.lab03.square.GoSquare;
+import ch.heig.gen.lab03.square.Square;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class PieceTest {
 
+    @Test
+    public void pieceShouldReturnCorrectSquare() {
+
+        Square square = new GoSquare();
+        Piece piece = new Piece(square);
+
+        Assertions.assertEquals(square, piece.getLocation());
+    }
 }
